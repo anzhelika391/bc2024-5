99bc3c94296adaedd82966301c1ddb64e9cc920c
Anzhelika bc2024-5 <s.anzhelika.r@gmail.com>
2024-11-18 01:08:43 +0200

    Add CLI parameters and web server functionality

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("Hello, bc2024-5!");
+const { Command } = require('commander');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// Ініціалізуємо Commander.js
+const program = new Command();
+
+program
+  .requiredOption('-h, --host <host>', 'Address of the server')
+  .requiredOption('-p, --port <port>', 'Port of the server')
+  .requiredOption('-c, --cache <cacheDir>', 'Path to the cache directory');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+// Перевіряємо, чи існує директорія кешу
+if (!fs.existsSync(options.cache)) {
+  console.error(`Error: Cache directory "${options.cache}" does not exist.`);
+  process.exit(1);
+}
+
+// Ініціалізуємо Express.js
+const app = express();
+
+// Веб-сервер на базі Express.js
+app.get('/', (req, res) => {
+  res.send('Hello! This is your server.');
+});
+
+// Запускаємо сервер
+app.listen(options.port, options.host, () => {
+  console.log(`Server is running at http://${options.host}:${options.port}`);
+});
